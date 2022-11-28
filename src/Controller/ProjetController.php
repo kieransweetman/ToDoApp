c122bc45eddbacae69652600e90a42fb7aa8ed7e
Fizzixx4 <gregory2koch@yahoo.fr>
2022-11-28 12:17:12 +0100

    affiche projet v2

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -5,19 +5,22 @@ namespace Digi\Todoapp\Controller;
 use Digi\Todoapp\Core\Views;
 use Digi\Todoapp\Model\Projets;
 use Digi\Todoapp\Model\Taches;
+use Digi\Todoapp\Model\Users;
 
 class ProjetController {
 
     public function __construct(){
         $this->AfficheProjets();
     }
-
+    //Page d'affichage de mes projets
     public function AfficheProjets(){
         $view = new Views('AfficheProjets','Liste des Projets');
         $projets = Projets::getAllOrderBy('id');
         $taches = Taches::getAllOrderBy('priorite');
+        $users = Users::getAll();
         $view->setVar('projets',$projets);
         $view->setVar('taches',$taches);
+        $view->setVar('users',$users);
         $view->setVar('TitrePage', 'Mes Projets');
         $view->render();
     }
