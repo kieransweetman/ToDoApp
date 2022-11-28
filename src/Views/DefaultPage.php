b676313314b7beedc28fb7764b44047c0e4e9274
ClemCoisc <clementcoiscaud@hotmail.fr>
2022-11-28 14:04:31 +0100

    Modif default page placeholder

--- src/Views/DefaultPage.php
+++ src/Views/DefaultPage.php
@@ -12,7 +12,7 @@ if ($connected != true) :
 
 ?>
     <form method='POST' action=''>
-        <input name='user' type='text' placeholder='Votre email'>
+        <input name='user' type='text' placeholder='Votre Pseudo'>
         <input name='pwd' type='password' placeholder="Votre mot de passe">
         <input type='submit' name='submit' value='Se connecter'>
     </form>
