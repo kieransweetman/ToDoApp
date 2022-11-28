15cfc4fc40b22f26738a0586250857b0b9df9887
Fizzixx4 <gregory2koch@yahoo.fr>
2022-11-28 19:17:36 +0100

    refacto et commentaire du code pour l'affichage des projets v1

--- src/Views/AfficheProjets.php
+++ src/Views/AfficheProjets.php
@@ -1,50 +1,33 @@
 <?php
-echo "<a href='index.php?page=" .
-    $_GET['page'] .
-    "&insert=projet'>Créer un nouveau projet</a><br>";
+echo "<a href='index.php?page=".$_GET['page']."&insert=projet'>Créer un nouveau projet</a>";
 echo '<h2>Liste de mes projets</h2>';
+
+//Boucle sur les affectations
 foreach($affectations as $affectation){
     //Boucle pour afficher les projets
     foreach ($projets as $projet) {
-        if($_SESSION['id'] === $affectation->getId_users() && $affectation->getId_projets() === $projet->getId() && $affectation->getAdmin() == 1){
+        //Condition pour afficher seulement les projets pour lesquels l'utilisateur courant est administrateur
+        if($_SESSION['id'] === $affectation->getId_users() && $affectation->getId_projets() === $projet->getId() && $affectation->getAdmin() === true){
             echo $projet->getLibelle();
-            echo "<a href='index.php?page=" .
-                $_GET['page'] .
-                '&update=' .
-                $projet->getId() .
-                "'>Modifier</a>";
-            echo "<a href='index.php?page=" .
-                $_GET['page'] .
-                '&delete=' .
-                $projet->getId() .
-                "'>Supprimer</a> ";
-            echo "<a href='index.php?page=" .
-                $_GET['page'] .
-                '&insert=tache' .
-                $projet->getId() .
-                "'>Ajouter une tâche</a><br>";
+            echo "<a href='index.php?page=".$_GET['page'].'&update='.$projet->getId()."'>Modifier</a>";
+            echo "<a href='index.php?page=".$_GET['page'].'&delete='.$projet->getId()."'>Supprimer</a> ";
+            echo "<a href='index.php?page=".$_GET['page'].'&insert=tache'.$projet->getId()."'>Ajouter une tâche</a><br>";
             //Boucle pour afficher les tâches
             foreach ($taches as $tache) {
+                //Condition pour afficher les tâches reliées au projet en cours
                 if ($tache->id_projets === $projet->getId()) {
                     echo $tache->getTitre() . ' ';
                     echo $tache->getPriorite() . ' ';
                     echo $tache->getStatut() . ' ';
                     //Boucle pour afficher les users
                     foreach ($users as $user) {
+                        //Condition pour afficher l'utilisateur relié à la tâche en cours
                         if ($tache->id_users === $user->getId()) {
                             echo $user->getPseudo();
                         }
                     }
-                    echo "<a href='index.php?page=" .
-                        $_GET['page'] .
-                        '&update=tache' .
-                        $tache->getId() .
-                        "'>Modifier</a> ";
-                    echo "<a href='index.php?page=" .
-                        $_GET['page'] .
-                        '&delete=tache' .
-                        $tache->getId() .
-                        "'>Supprimer</a> ";
+                    echo "<a href='index.php?page=".$_GET['page'].'&update=tache'.$tache->getId()."'>Modifier</a> ";
+                    echo "<a href='index.php?page=".$_GET['page'].'&delete=tache'.$tache->getId()."'>Supprimer</a> ";
                     echo '<br>';
                 }
             }
