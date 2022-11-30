ada2cdc10398caa2f4c33a06290fd7e2e45cf302
ClemCoisc <clementcoiscaud@hotmail.fr>
2022-11-30 09:44:10 +0100

    Fonction is valid avec fonction intersect

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -10,17 +10,19 @@ use Digi\Todoapp\Model\Projets;
 use Digi\Todoapp\Model\Taches;
 use Digi\Todoapp\Model\Users;
 
-class ProjetController {
+class ProjetController
+{
 
     //Constructeur de la page ProjetController
-    public function __construct(){
+    public function __construct()
+    {
         if (isset($_GET['delete'])) {
-            if(isset($_POST['oui'])){
-                Taches::deleteByAttribute('id_projets',$_GET['delete']);
-                Affectation::deleteByAttribute('id_projets',$_GET['delete']);
+            if (isset($_POST['oui'])) {
+                Taches::deleteByAttribute('id_projets', $_GET['delete']);
+                Affectation::deleteByAttribute('id_projets', $_GET['delete']);
                 Projets::deleteById((int)$_GET['delete']);
             }
-            if(isset($_POST['non'])){
+            if (isset($_POST['non'])) {
                 //Pour recharger la page en cours et faire disparaître le form de l'affichage
                 header('location: index.php?page=afficheprojets');
             }
@@ -28,19 +30,18 @@ class ProjetController {
         }
         if (isset($_GET['insert'])) {
             $this->CreateProjet();
-        }
-        elseif(isset($_GET['update'])){
+        } elseif (isset($_GET['update'])) {
             $this->UpdateProjet();
-        }
-        else{
+        } else {
             $this->AfficheProjets();
         }
     }
 
     //Page d'affichage de mes projets
-    public function AfficheProjets(){
+    public function AfficheProjets()
+    {
         //Création de la vue
-        $view = new Views('AfficheProjets','Liste des Projets');
+        $view = new Views('AfficheProjets', 'Liste des Projets');
         //Vérification et maintien de la session, sinon retour à l'accueil
         if (Security::isConnected()) {
             $view->setVar('connected', true);
@@ -50,22 +51,23 @@ class ProjetController {
         //On va cherchez les projets, les tâches, les users et les affectations de la base de données
         $projets = Projets::getAllOrderBy('id');
         $taches = Taches::getAllOrderBy('priorite');
-        $users = Users::getAll();//Voir pour sortir que l'utilisateur de la session
+        $users = Users::getAll(); //Voir pour sortir que l'utilisateur de la session
         $affectations = Affectation::getAll();
         //On assigne nos résultats au tableau de setVar pour les récupérer sur la vue
-        $view->setVar('projets',$projets);
-        $view->setVar('taches',$taches);
-        $view->setVar('users',$users);
-        $view->setVar('affectations',$affectations);
+        $view->setVar('projets', $projets);
+        $view->setVar('taches', $taches);
+        $view->setVar('users', $users);
+        $view->setVar('affectations', $affectations);
         $view->setVar('TitrePage', 'Mes Projets');
         //On crée et on affiche la page
         $view->render();
     }
 
     //Page de création de projet
-    public function CreateProjet(){
+    public function CreateProjet()
+    {
         //Création de la vue
-        $view = new Views('CreateUpdateProjets','Création d\'un projet');
+        $view = new Views('CreateUpdateProjets', 'Création d\'un projet');
         //Vérification et maintien de la session, sinon retour à l'accueil
         if (Security::isConnected()) {
             $view->setVar('connected', true);
@@ -74,39 +76,38 @@ class ProjetController {
         }
         //Ajout de variables au tableau setVar pour les récupérer sur la View
         $view->setVar('TitrePage', 'Création de projet');
-        $view->setVar('action','&insert=projet');
+        $view->setVar('action', '&insert=projet');
         $view->setVar('submit', 'Créer projet');
         if (isset($_POST['create'])) {
-            if(($message=$this->isValidCreate()) === ''){
-                if(Projets::create()) {
-                    Affectation::createAffectation(Projets::getLastId(), $_SESSION['id'],'1');
-                    $view->setVar('message','Un projet a bien été créé');
+            if (($message = $this->isValidCreate()) === '') {
+                if (Projets::create()) {
+                    Affectation::createAffectation(Projets::getLastId(), $_SESSION['id'], '1');
+                    $view->setVar('message', 'Un projet a bien été créé');
                 } else {
                     $view->setVar('message', 'Une erreur est survenue!');
                 }
+            } else {
+                $view->setVar('message', $message);
             }
-            else{
-                $view->setVar('message',$message);
-            }
-            $view->setVar('libelle',$_POST['libelle']);
+            $view->setVar('libelle', $_POST['libelle']);
         }
-        if(($message1 = $this->isValidAddUser()) === ''){
-            if(isset($_POST['pseudo'])){
+        if (($message1 = $this->isValidAddUser()) === '') {
+            if (isset($_POST['pseudo'])) {
                 $user = Users::getByAttribute('pseudo', $_POST['pseudo']);
-                $id_user= $user[0]->getId();
-                Affectation::createAffectation(Projets::getLastId(), $id_user,'0');
-                $view->setVar('message1','L\'utilisateur a bien été ajouté');
+                $id_user = $user[0]->getId();
+                Affectation::createAffectation(Projets::getLastId(), $id_user, '0');
+                $view->setVar('message1', 'L\'utilisateur a bien été ajouté');
             }
-        }
-        else{
-            $view->setVar('message1',$message1);
+        } else {
+            $view->setVar('message1', $message1);
         }
         $view->render();
     }
 
-    public function UpdateProjet(){
+    public function UpdateProjet()
+    {
         //Création de la vue
-        $view = new Views('CreateUpdateProjets','Modification d\'un projet');
+        $view = new Views('CreateUpdateProjets', 'Modification d\'un projet');
         //Vérification et maintien de la session, sinon retour à l'accueil
         if (Security::isConnected()) {
             $view->setVar('connected', true);
@@ -115,40 +116,55 @@ class ProjetController {
         }
         //Ajout de variables au tableau setVar pour les récupérer sur la View
         $view->setVar('TitrePage', 'Modification de projet');
-        $view->setVar('action',"&update=".$_GET['update']);
+        $view->setVar('action', "&update=" . $_GET['update']);
         $view->setVar('submit', 'Modifier');
         $projet = Projets::getById($_GET['update']);
-        $view->setVar('libelle',$projet[0]->getLibelle());
+        $view->setVar('libelle', $projet[0]->getLibelle());
         if (isset($_POST['create'])) {
-            if(($message=$this->isValidUpdate()) === ''){
-                if(Projets::updateById()) {
-                    $view->setVar('message','Un projet a été modifié');
+            if (($message = $this->isValidUpdate()) === '') {
+                if (Projets::updateById()) {
+                    $view->setVar('message', 'Un projet a été modifié');
                 }
-                $view->setVar('libelle',$_POST['libelle']);
-            }
-            else{
-                $view->setVar('message',$message);
+                $view->setVar('libelle', $_POST['libelle']);
+            } else {
+                $view->setVar('message', $message);
             }
         }
         $view->render();
     }
 
     //Test de validation pour éviter les doublons de libellé en création
-    private function isValidCreate(){
-        $return ='';
-        $projets = Projets::getByAttribute('libelle',$_POST['libelle']);
-        if(count($projets)>0){
+    private function isValidCreate()
+    {
+        $return = '';
+        $projets = Projets::getByAttribute('libelle', $_POST['libelle']);
+        if (count($projets) > 0) {
             $return = "Le libellé de ce projet existe déjà";
         }
         return $return;
     }
 
     //Test de validation pour éviter les doublons de libellé en modification
-    private function isValidUpdate(){
-        $return ='';
-        $projets = Projets::getByAttribute('libelle',$_POST['libelle']);
-        $affectation = Affectation::getByAttribute('id_projets', $_GET['update']);
-        if(count($projets)>0 && $_SESSION['id'] !== $affectation[0]->getId_users()){
+    private function isValidUpdate()
+    {
+        $return = '';
+        $projets = Projets::getByAttribute('libelle', $_POST['libelle']);
+        $affectation = Affectation::getByAttribute('id_users', $_SESSION['id']);
+        //Stock tous les id Projet ayant le meme libelle que le formulaire post dans un tableau
+        $tableauLibelle=[];
+        foreach ($projets as $projet){
+            $tableauLibelle[] = $projet->getId();
+        }
+        //Stock tous les id Projet de l'utilisateur dans un tableau
+        $tableauUser=[];
+        foreach ($affectation as $affect){
+            $tableauUser[] = $affect->getId_projets();
+        }
+        //Créé un troisieme tableau qui ressort les id commun entre le tableauUser et le TableaiLibelle. Et donc si il y a un libellé identique sur le compte de l'utilisateur
+        $intersect = [];
+        $intersect = array_intersect($tableauLibelle,$tableauUser);
+        //Si le tableau intersect n'est pas vide, le libellé exite déja sur le compte de l'utilisateur et donc on montre le message d erreur du doublon
+        if (count($projets) > 0 && !empty($intersect)) {
             $return = "Le libellé de ce projet existe déjà";
         }
         return $return;
@@ -158,13 +174,12 @@ class ProjetController {
     {
         $return = '';
         // Validation du formulaire si le pseudo n'est pas dans la BDD 
-        if(isset($_POST['pseudo'])){
+        if (isset($_POST['pseudo'])) {
             $pseudo = Users::getByAttribute('pseudo', $_POST['pseudo']);
-        if (count($pseudo) == 0) {
-            $return .= "Veuillez créer l'utilisateur<br>";
-        }
+            if (count($pseudo) == 0) {
+                $return .= "Veuillez créer l'utilisateur<br>";
+            }
         }
         return $return;
     }
-
 }
